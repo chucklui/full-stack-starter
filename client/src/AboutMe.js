ec7c870a578b40fc2a471333bf2521910904a8ec
Chuck Lui <luialex0@gmail.com>
2021-02-08 19:12:32 -0800

    Updated a new image

--- client/src/AboutMe.js
+++ client/src/AboutMe.js
@@ -1,10 +1,10 @@
 import './AboutMe.scss';
-import me from './me.jpg';
+import me2 from './me2.jpg';
 
 function AboutMe() {
   return (
     <div id="about-me">
-      <img className="img-fluid mb-3" src={me} alt="My Name" />
+      <img className="img-fluid mb-3" src={me2} alt="My Name" />
       <h1>Chuck Lui</h1>
       <p>Hi! My name is Cheuk Ho Lui but I go by Chuck. I am a recent graduate student from UCM majoring in Bioengineering. Throughout college I was involved in culture clubs as well as Greek community. I'm passion about making an impact on the world through advacned technology and creativity.</p>
     </div>
